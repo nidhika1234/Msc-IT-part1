619513ee0b543fe24460c625797e9af3f6895a88
nidhika1234 <142785894+nidhika1234@users.noreply.github.com>
2024-03-15 16:30:44 +0530

    Add files via upload

---
+++ Main1 (1).py
@@ -0,0 +1,7 @@
+def Add(a,d):
+    return a+b
+def Display():
+    print("Hello")
+
+if __name__=='__main__':
+   Display()
---
+++ Test.py
@@ -0,0 +1,8 @@
+from Main1 import Add
+
+def TestAdd():
+  assert Add (3,4)==9
+  print("Add Function works correctly")
+  
+  if__main__=='__main__':
+    TestAdd()
